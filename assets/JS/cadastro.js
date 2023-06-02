656e978eb69da6a6bb66e6c5f3ed408126662d88
Thiago <85202034+Psycojoee@users.noreply.github.com>
2023-06-02 15:52:22 -0300

    3333

--- assets/JS/cadastro.js
+++ assets/JS/cadastro.js
@@ -48,7 +48,7 @@ function entrar() {
   }
 
   if (usuario.value == userValid.user && senha.value == userValid.senha) {
-    window.location.href = 'index.html'
+    window.location.href = 'https://psycojoee.github.io/GS_ADVANCED_PROGRAMMING/'
 
     let token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
     localStorage.setItem('token', token)
