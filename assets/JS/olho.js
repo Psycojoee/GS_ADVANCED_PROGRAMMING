3763335c38b9a2d1b0de03275bb845e868155c40
Thiago <marthi480@gmail.com>
2023-06-02 12:52:57 -0300

    Tudo funcional

--- assets/JS/olho.js
+++ assets/JS/olho.js
@@ -1,62 +1,66 @@
 let btn = document.querySelector('.fa-eye')
 
-btn.addEventListener('click', ()=>{
-    let inputSenha = document.querySelector('#senha')
+btn.addEventListener('click', () => {
+  let inputSenha = document.querySelector('#senha')
 
-    if(inputSenha.getAttribute('type') == 'password'){
-        inputSenha.setAttribute ('type', 'text')        
-    }
-    else {
-        inputSenha.setAttribute ('type', 'password')
-    }
+  if (inputSenha.getAttribute('type') == 'password') {
+    inputSenha.setAttribute('type', 'text')
+  } else {
+    inputSenha.setAttribute('type', 'password')
+  }
 })
 
+function entrar() {
+  let usuario = document.querySelector('#usuario')
+  let userLabel = document.querySelector('#userLabel')
 
-function entrar(){
-    let usuario = document.querySelector('#usuario')
-    let userLabel = document.querySelector('#userLabel')
+  let senha = document.querySelector('#senha')
+  let senhaLabel = document.querySelector('#senhaLabel')
 
-    let senha = document.querySelector('#senha')
-    let senhaLabel = document.querySelector('#senhaLabel')
+  let msgError = document.querySelector('#msgError')
+  let listaUser = []
 
-    let msgError = document.querySelector('#msgError')
-    let listaUser = []
+  let userValid = {
+    nome: '',
+    user: '',
+    senha: ''
+  }
 
-    let userValid = {
-        nome: '',
-        user: '',
-        senha: ''
-    }
+  listaUser = JSON.parse(localStorage.getItem('listaUser'))
 
-    listaUser = JSON.parse(localStorage.getItem('listaUser'))
-
-    if (listaUser !== null) {
-        listaUser.forEach((item) => {
-          if(usuario.value == item.userCad && senha.value == item.senhaCad){
-            userValid = {
-              nome: item.nomeCad,
-              user: item.userCad,
-              senha: item.senhaCad
-            };
-          }
-        });
+  if (listaUser !== null) {
+    listaUser.forEach((item) => {
+      if (usuario.value == item.userCad && senha.value == item.senhaCad) {
+        userValid = {
+          nome: item.nomeCad,
+          user: item.userCad,
+          senha: item.senhaCad
+        }
       }
+    })
+  }
 
-    if(usuario.value == userValid.user && senha.value == userValid.senha){
-        window.location.href = 'http://127.0.0.1:5500/HTML/ficha.html'
+  if (usuario.value === '' || senha.value === '') {
+    msgError.setAttribute('style', 'display:block')
+    msgError.innerHTML = 'Preencha todos os campos'
+    usuario.focus()
+    return // Retorna e não prossegue com o login se os campos estiverem vazios
+  }
 
-        let token = Math.random().toString (16).substring(2) + Math.random().toString(16).substring(2)
-        localStorage.setItem('token', token)
+  if (usuario.value == userValid.user && senha.value == userValid.senha) {
+    window.location.href = 'http://127.0.0.1:5500/HTML/ficha.html'
 
-        localStorage.setItem('userLogado', JSON.stringify(userValid))
-    } else {
-        usuario.setAttribute('style', 'border-color: red')
-        userLabel.setAttribute('style', 'color: red')
-        senha.setAttribute('style', 'border-color: red')
-        senhaLabel.setAttribute('style', 'color: red')
-        msgError.setAttribute ('style', 'display:block')
-        msgError.innerHTML = 'Usuário ou senha incorretos'
-        usuario.focus()
-    }
+    let token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
+    localStorage.setItem('token', token)
 
+    localStorage.setItem('userLogado', JSON.stringify(userValid))
+  } else {
+    usuario.setAttribute('style', 'border-color: red')
+    userLabel.setAttribute('style', 'color: red')
+    senha.setAttribute('style', 'border-color: red')
+    senhaLabel.setAttribute('style', 'color: red')
+    msgError.setAttribute('style', 'display:block')
+    msgError.innerHTML = 'Usuário ou senha incorretos'
+    usuario.focus()
+  }
 }
