09d0ba89c8bce318f257af9a6a9dc90000266734
Thiago <85202034+Psycojoee@users.noreply.github.com>
2023-06-02 17:00:17 -0300

    1111

--- assets/JS/olho2.js
+++ assets/JS/olho2.js
@@ -98,7 +98,7 @@ function cadastrar(){
         msgError.innerHTML = ''
 
         setTimeout(()=>{
-            window.location.href = 'GS_ADVANCED_PROGRAMMING/index.html'   
+            window.location.href = '.\index.html'   
         }, 3000)
         
 
